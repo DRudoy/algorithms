7623882b55652db324215ebd40187bcdff7e742e
Dima <rudoydmitrygit@yandex.ru>
2018-02-05 14:36:22 +0300

    Добавлена домашнее 3 недели

---
+++ hse/3week/MergeSort/main.cpp
@@ -0,0 +1,91 @@
+﻿#include "ReadWriter.h"
+#include "MergeSort.h"
+
+using namespace std;
+
+int main()
+{
+    ReadWriter rw;
+
+
+    int length;
+
+    //Read length from file
+    length = rw.readInt();
+
+    int* brr = new int[length];
+    rw.readArray(brr, length);
+
+    //Start sorting
+    MergeSort s;
+
+    s.sort(brr, length);
+
+    //Write answer to file
+    rw.writeArray(brr, length);
+
+    delete[] brr;
+
+    return 0;
+}
+
+void MergeSort::sort(int *arr, int length)
+{
+	divide_and_merge(arr, 0, (length-1));
+}
+
+
+void MergeSort::merge(int* arr, int first, int second, int end)
+{
+
+   int brr[end];
+   int startA=first;
+   int startB=second+1;
+   for (int i = first; i <=end; ++i)
+   {
+	   brr[i]=arr[i];
+   }
+   for (int i = startA; i <=end; ++i)
+   {
+	   if((startA > second) || (startB > end))
+	   {
+		   if(startB > end)
+		   {
+			   arr[i] = brr[startA];
+			   startA++;
+		   }
+		   else
+		   {
+			   arr[i] = brr[startB];
+			   startB++;
+		   }
+	   }
+	   else
+	   {
+		   if(brr[startA] <= brr[startB])
+		   {
+			   arr[i] = brr[startA];
+			   startA++;
+		   }
+		   else
+		   {
+			   arr[i] = brr[startB];
+			   startB++;
+		   }
+	   }
+
+   }
+}
+
+
+void MergeSort::divide_and_merge(int *arr, int left, int right)
+{
+	int middle;
+	if(left<right)
+	{
+		middle=(left+right)/2;
+		divide_and_merge(arr, left, middle);
+		divide_and_merge(arr, (middle+1), right);
+		merge(arr,left,middle,right);
+	}
+}
---
+++ hse/3week/WinterSkies/main.cpp
@@ -0,0 +1,148 @@
+﻿#include "Solver.h"
+using namespace std;
+void change(Student* students, Ski* skis,int low,int height)
+{
+	Student pivot=students[low];
+	Student temp;
+	Ski tempSki;
+	int i=low;
+	int j=height;
+	while(true)
+	{
+		while(students[i].id < pivot.id)
+			i++;
+		while(students[j].id > pivot.id)
+			j--;
+		if(i<=j)
+		{
+			//swap
+			temp=students[i];
+			students[i]=students[j];
+			students[j]=temp;
+
+			//swap
+			tempSki=skis[i];
+			skis[i]=skis[j];
+			skis[j]=tempSki;
+			i++;
+			j--;
+		}
+		if(i>j)
+			break;
+	}
+
+	if(low<j)
+		change(students, skis, low, j);
+	if(i<height)
+		change(students, skis, i, height);
+
+}
+void quickSortForStudent(Student * students,Ski* skis,int low,int height)
+{
+	Student pivotStudent;
+	Ski pivotSkies = skis[low];
+	for (int m = low; m <= height; ++m)
+	{
+		if(compare(students[m], pivotSkies)==0)
+		{
+			pivotStudent=students[m];
+			swap(students[m],students[low]);
+			break;
+		}
+	}
+	int i = low+1;
+	int j = height;
+	while(true)
+	{
+		while(compare(pivotStudent,skis[i])==-1)
+		{
+			if(i <= height)
+				i++;
+			else
+				break;
+		}
+
+		while(compare(pivotStudent,skis[j])==1)
+		{
+			if(j >= (low+1))
+				j--;
+			else
+				break;
+		}
+		if(i <= j)
+		{
+			swap(skis[i],skis[j]);
+			i++;
+			j--;
+		}
+		if(i>j)
+			break;
+	}
+	swap(skis[low],skis[j]);
+	i =low+1;
+	j = height;
+	while(true)
+	{
+		while(compare(students[i],pivotSkies)==1)
+		{
+			if(i<=height)
+				i++;
+			else
+				break;
+		}
+
+		while(compare(students[j],pivotSkies)==-1)
+		{
+			if(j>=(low+1))
+				j--;
+			else
+				break;
+		}
+		if(i <= j)
+		{
+			swap(students[i],students[j]);
+			i++;
+			j--;
+		}
+		if(i>j)
+			break;
+	}
+	swap(students[low],students[j]);
+	if(low<j)
+		quickSortForStudent(students, skis, low, j);
+	if(height>i)
+		quickSortForStudent(students, skis, i, height);
+}
+int main()
+{
+    ReadWriter rw;
+
+    int n;
+
+    //Read n from file
+    n = rw.readInt();
+
+    //Create arrays
+    Student* students = new Student[n];
+    Ski* skis = new Ski[n];
+
+    //read Students and Skis from file
+    rw.readStudents(students, n);
+    rw.readSkis(skis, n);
+
+    //Find pairs
+    Solver s;
+    s.findPairs(students, skis, n);
+
+    //Write answer to file
+    rw.writeStudentsAndSkis(students, skis, n);
+
+    delete[] students;
+    delete[] skis;
+    return 0;
+}
+void Solver::findPairs(Student* students, Ski* skis, int n)
+{
+	quickSortForStudent(students, skis, 0, n-1);
+	change(students, skis,0,n-1);
+}
